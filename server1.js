7edb7e0a4eddd0c14d217fa40da436f8be1dc64b
noruga <31901697+noruga@users.noreply.github.com>
2018-08-30 20:39:55 +0100

    Add files via upload

--- server1.js
+++ server1.js
@@ -271,14 +271,14 @@ io.on('connection', function (socket) {
             var puckDist = ((data[0].puckX - puckX)*(data[0].puckX - puckX) + (data[0].puckY - puckY)*(data[0].puckY - puckY));
             puckX = data[0].puckX;
             puckY = data[0].puckY;
-            if (puckDist < 4)
+            if (puckDist < 2)
                 puckSlowCount++;
             else
                 puckSlowCount = 0;
 
             if (adversoryDist < myDist){// && faceOffCounter == 0){         //faceOffCounter : if goal has been scored last within last 2 secs
                 countHost++;
-                if (((countHost > 20) && (puckSlowCount > 12))){
+                if (((countHost > 25) && (puckSlowCount > 6))){
                     countHost = 0;
                     puckSlowCount = 0;
                     lastHost = !players[playerId].host;
