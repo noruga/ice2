8ff94724aebb68b6385bb92d4f370f31a7e0eb2a
noruga <31901697+noruga@users.noreply.github.com>
2019-10-21 21:15:06 +0100

    Add files via upload

--- client/js/Boot.js
+++ client/js/Boot.js
@@ -154,9 +154,8 @@ if((_this.playerSprites !== undefined)  || (_this.playerSprites !== null)){
                 /*if (_this.playerSprites[data[i].id][0].pushedPlay)
                     _this.pushedPlayer = 0;
                 else
-                    _this.pushedPlayer = 1;*/
-
-                if (data[i].pushed > 0){
+    /*                _this.pushedPlayer = 1;*/
+/*    if (data[i].pushed > 0){
                     console.log(data[i].pushed, "push number")
                     _this.playerSprites[socket.id][pushedPlay].body.velocity.x += Math.cos(_this.playerSprites[data[i].id][data[i].pushingPlay].target_rotation) * data[i].pushed*10;
                     _this.playerSprites[socket.id][pushedPlay].body.velocity.y += Math.sin(_this.playerSprites[data[i].id][data[i].pushingPlay].target_rotation) * data[i].pushed*10;
@@ -217,7 +216,7 @@ setInterval(function () {
 
 }, emitRate);
 
-function preparePlayersDataToSend() {
+function preparePlayersDataToSend() {/*
     for (var id in _this.playerSprites) {
         if (id !== socket.id){
             if (_this.playerSprites[id][0].pushedPlayer)
@@ -232,7 +231,7 @@ function preparePlayersDataToSend() {
                 pushingPlayer = 1;
         }
     }
-
+*/
     var dataToSend = [];
    /* dataToSend.push({id: socket.id, left: left, right: right, down: down, up: up, brake: brake, shoot: shoot, go_home: go_home, 
         controlPlayer0: _this.playerSprites[socket.id][0].controlPlayer, puckX: _this.puck.x, puckY: _this.puck.y, host: host});*/
